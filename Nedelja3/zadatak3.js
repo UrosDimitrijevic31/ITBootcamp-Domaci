398f1fae6b0c396726d8772f2c7759ec46b362b8
UrosDimitrijevic31 <urosrgf@gmail.com>
2019-11-08 22:01:04 +0100

    uradjen domaci zadat u cetvrtak

---
+++ Nedelja3/zadatak3.js
@@ -0,0 +1,12 @@
+/* Написати функцију која исписује све елементе низа који су дељиви са 5 */
+
+function divisibleByFive(arr){
+    arr.forEach(element => {
+        if(element % 5 == 0) {
+            console.log(element);
+        }
+    });
+}
+
+divisibleByFive([1,2,3,4,5,10,15,23,1,5]) // Исписује -> 5,10,15,5
+
